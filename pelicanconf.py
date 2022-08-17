298bae79565eabb22a2328d1dd195e5124acd0f9
renyuneyun (Rui Zhao) <renyuneyun@gmail.com>
2022-08-17 18:27:03 +0100

    Updated title

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Rui Zhao'
-SITENAME = "Rui Zhao's research homepage"
+SITENAME = "Rui Zhao's research"
 SITEURL = 'http://localhost:8000'
 
 PATH = 'content'
